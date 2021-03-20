99c8cb80f18f468bf1f3dc235b9e663df8e0d1f5
Ragnhild Steinsland <ragnhild98@live.no>
2021-03-20 23:42:10 +0100

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Ragnhild Kjellevold Steinsland on 20/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
